ead956aa109913b4bd405a2016055cdf1916955a
arkStudent <arkstudents@secab.org>
2024-06-21 09:44:02 +0530

    Lubna: added the method to show attendance

--- app/Http/Controllers/AttendController.php
+++ app/Http/Controllers/AttendController.php
@@ -10,4 +10,27 @@ class AttendController extends Controller
 
         return view('attendance');
     }
+
+    public function showAttendance(Request $request)
+    {
+        $request->validate([
+            'fdate' => 'required|date',
+            'tdate' => 'required|date|after_or_equal:fdate',
+        ]);
+
+        $fdate = $request->input('fdate');
+        $tdate = $request->input('tdate');
+        $student_id = Session::get('student_id');
+        $std = Session::get('std');
+        $dv = Session::get('dv');
+
+        $attendance = DB::table('attendance')
+            ->where('student_id', $student_id)
+            ->where('std', $std)
+            ->where('dv', $dv)
+            ->whereBetween('date', [$fdate, $tdate])
+            ->get();
+
+        return response()->json($attendance);
+    }
 }
