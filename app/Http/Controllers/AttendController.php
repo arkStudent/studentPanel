6c1bffa5c54f552886dd03bf9cf6201ce119e548
arkStudent <arkstudents@secab.org>
2024-06-21 16:59:28 +0530

    Lubna: removed the code to show attendance table

--- app/Http/Controllers/AttendController.php
+++ app/Http/Controllers/AttendController.php
@@ -13,31 +13,5 @@ class AttendController extends Controller
         return view('attendance');
     }
 
-    public function showAttendance(Request $request)
-{
-    $request->validate([
-        'fdate' => 'required|date',
-        'tdate' => 'required|date|after_or_equal:fdate',
-    ]);
-
-    $fdate = $request->input('fdate');
-    $tdate = $request->input('tdate');
-    $student_id = Session::get('student_id');
-    $std = Session::get('std');
-    $dv = Session::get('dv');
-
-    $attendance = DB::table('ark_attend')
-        ->where('student_id', $student_id)
-        ->where('std', $std)
-        ->where('dv', $dv)
-        ->whereBetween('date', [$fdate, $tdate])
-        ->get();
-
-    if ($request->ajax()) {
-        return response()->json($attendance);
-    } else {
-        return view('attendTable', compact('attendance'));
-    }
-}
 
 }
