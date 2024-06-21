63d266d713019db3b9ed6a64940fcd7171339ce3
arkStudent <arkstudents@secab.org>
2024-06-21 16:59:55 +0530

    Lubna: removed the code to show attendance table

--- resources/views/attendTable.blade.php
+++ resources/views/attendTable.blade.php
@@ -1,29 +1,17 @@
 @extends('index')
 
 @section('content')
-    <div class="container">
-        <h5 class="text-center">Attendance Details</h5>
-        <div id="attendanceTable">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>sl.no</th>
-                        <th>date</th>
-                        <th>dayname</th>
-                        <th>attn</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($attendance as $index => $item)
-                        <tr>
-                            <td>{{ $index + 1 }}</td>
-                            <td>{{ \Carbon\Carbon::parse($item->date)->toDateString() }}</td>
-                            <td>{{ \Carbon\Carbon::parse($item->date)->locale('en-US')->dayName }}</td>
-                            <td>{{ $item->atn == 1 ? 'present' : 'absent' }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+<table class="table">
+    <thead>
+        <tr>
+            <th>sl.no</th>
+            <th>date</th>
+            <th>dayname</th>
+            <th>attn</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
 @endsection
