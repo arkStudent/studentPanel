1930e293296c9441af80ba40df086ec80b393b5b
arkStudent <arkstudents@secab.org>
2024-06-21 16:15:14 +0530

    Lubna: removed fetch method to request api and added axios method

--- app/Http/Controllers/AttendController.php
+++ app/Http/Controllers/AttendController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class AttendController extends Controller
 {
@@ -12,25 +14,30 @@ class AttendController extends Controller
     }
 
     public function showAttendance(Request $request)
-    {
-        $request->validate([
-            'fdate' => 'required|date',
-            'tdate' => 'required|date|after_or_equal:fdate',
-        ]);
+{
+    $request->validate([
+        'fdate' => 'required|date',
+        'tdate' => 'required|date|after_or_equal:fdate',
+    ]);
 
-        $fdate = $request->input('fdate');
-        $tdate = $request->input('tdate');
-        $student_id = Session::get('student_id');
-        $std = Session::get('std');
-        $dv = Session::get('dv');
+    $fdate = $request->input('fdate');
+    $tdate = $request->input('tdate');
+    $student_id = Session::get('student_id');
+    $std = Session::get('std');
+    $dv = Session::get('dv');
 
-        $attendance = DB::table('attendance')
-            ->where('student_id', $student_id)
-            ->where('std', $std)
-            ->where('dv', $dv)
-            ->whereBetween('date', [$fdate, $tdate])
-            ->get();
+    $attendance = DB::table('ark_attend')
+        ->where('student_id', $student_id)
+        ->where('std', $std)
+        ->where('dv', $dv)
+        ->whereBetween('date', [$fdate, $tdate])
+        ->get();
 
+    if ($request->ajax()) {
         return response()->json($attendance);
+    } else {
+        return view('attendTable', compact('attendance'));
     }
+}
+
 }
