1f8b1fe9a7ab838c150bd1784fbd41e249b604f3
arkStudent <arkstudents@secab.org>
2024-06-22 16:39:05 +0530

    Lubna: added table code and data to show attendance

--- resources/views/attendTable.blade.php
+++ resources/views/attendTable.blade.php
@@ -1,17 +1,24 @@
 @extends('index')
 
 @section('content')
-<table class="table">
-    <thead>
-        <tr>
-            <th>sl.no</th>
-            <th>date</th>
-            <th>dayname</th>
-            <th>attn</th>
-        </tr>
-    </thead>
-    <tbody>
-    </tbody>
-</table>
-
+    <table class="table table-striped table-bordered text-center">
+        <thead class="thead-dark">
+            <tr>
+                <th>Sl.no</th>
+                <th>Date</th>
+                <th>Day</th>
+                <th>Attendance</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($attendance as $index => $record)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $record->date }}</td>
+                    <td>{{ $record->std }}</td>
+                    <td>{{ $record->atn === 1 ? 'Present' : 'Absent'}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
